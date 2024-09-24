f2e0db9a6f5fdbab4e0d3a84aa0b0c6edb7596ae
stealthman22 <alaraojoel@gmail.com>
2024-09-24 00:28:10 +0000

    built hello world

--- week-0/hello.py
+++ week-0/hello.py
@@ -1,1 +1,17 @@
-print("hello, world!")
+#print("What's your name? ")
+# Ask user for their name
+name = input("What's your name? ")
+# Say hello to user
+# The + sign here is called concatenation
+#print("Hello " +  name)
+
+#Another way
+#When you pass multiple arguments into print,
+# It automatically creates a space.
+print("Hello,", name)
+"""
+This can also be used 
+for multiline comments
+"""
+
+# New line
