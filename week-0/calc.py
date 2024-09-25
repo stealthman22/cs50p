0cd9d36c691f27dbf13ffc2630fbc29003abe847
stealthman22 <alaraojoel@gmail.com>
2024-09-25 01:45:50 +0000

    built calculator myself

---
+++ week-0/calc.py
@@ -0,0 +1,10 @@
+x = 1
+y =2
+z= x + y
+print( z)
+
+# Trying David's next point myself
+x =  int( input("What is your first numer? "))
+y=  int(input("What is your second numer? ")) 
+z = x + y
+print( z)
