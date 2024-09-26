860961e7b9376272c6967c884dec5b3d2302c2bb
stealthman22 <alaraojoel@gmail.com>
2024-09-26 11:14:29 +0000

    using f strings

--- week-0/calc.py
+++ week-0/calc.py
@@ -4,7 +4,28 @@ z= x + y
 print( z)
 
 # Trying David's next point myself
-x =  int( input("What is your first numer? "))
-y=  int(input("What is your second numer? ")) 
-z = x + y
-print( z)
+# x =  int( input("What is your first numer? "))
+# y=  int(input("What is your second numer? ")) 
+# z = x + y 
+# print( x + y)
+
+#David's Solution
+# x = input("What is your first numer? ")
+# y=input("What is your second numer? ")
+
+# z = int(x) + int(y)
+# print( z)
+
+#  Let'd do it with floats
+# x =  float( input("What is your first numer? "))
+# y=  float(input("What is your second numer? ")) 
+# z = x + y 
+# print( x + y)
+
+
+
+x =  float( input("What is your first numer? "))
+y=  float(input("What is your second numer? ")) 
+
+z=round(x +y)
+print(f"{z}")
