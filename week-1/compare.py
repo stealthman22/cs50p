5780a1bc8eb3f98bb3914e3c0814d4b46d94a9c8
stealthman22 <alaraojoel@gmail.com>
2024-09-30 13:10:57 +0000

    done with compare.py logic

--- week-1/compare.py
+++ week-1/compare.py
@@ -25,8 +25,21 @@ y = int(input("What's y? "))
 # else:
 #     print("x is equal to y")
 
+# using the or keyword
+# if x < y or x > y:
+#     print("x is not equal to y")
+# else:
+#     print("x is equal to y")
 
-if x < y or x > y:
-    print("x is not equal to y")
-else:
+# if we are only concerned with equality of x and y, we can use the not equal to operator.
+
+# if x != y:
+#     print("x is not equal to y")
+# else:
+#     print("x is equal to y")
+
+# Now using only the equal to operator.
+if x == y:
     print("x is equal to y")
+else:
+    print("x is not equal to y")
