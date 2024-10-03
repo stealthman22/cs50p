41f357d79917aba3b8d4a78f8e8fdb6ef0a6acbf
stealthman22 <alaraojoel@gmail.com>
2024-10-03 03:03:14 +0000

    match statement

---
+++ week-1/house.py
@@ -0,0 +1,28 @@
+name = input("What's your name? ")
+
+# if name == "Harry":
+#     print("Gryffindor")
+# elif name == "Hermione":
+#     print("Gryffindor")
+# elif name == "Ron":
+#     print("Gryffindor")
+# elif name == "Draco":
+#     print("Slytherin")
+# else:
+#     print("Who?")
+
+# if name == "Harry" or name == "Hermoine" or name == "Ron":
+#     print("Gryffindor")
+# elif name == "Draco":
+#     print("Slytherin")
+# else:
+#     print("Who?")
+
+# Using the Match keyword
+match name:
+    case "Harry" | "Hermoine" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")
