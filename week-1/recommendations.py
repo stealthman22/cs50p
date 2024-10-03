18dbd697637fb5f7a5c07ca8d6a88c7a9b45b9f3
stealthman22 <alaraojoel@gmail.com>
2024-10-03 03:49:56 +0000

    recommendations

---
+++ week-1/recommendations.py
@@ -0,0 +1,26 @@
+#  Nesting conditional statement
+def main():
+    difficulty = input("Difficult or Casual? ")
+    players = input("Multiplayer or Single-Player? ")
+
+    if difficulty == "Difficut":
+        if players == "Multiplayer":
+            recommend("Poker")
+        elif players == "Single-Player":
+            recommend("Klondike")
+        else:
+            print("Enter a valid set of players")
+    elif difficulty == "Casual":
+        if players == "Multiplayer":
+            recommend("Hearts")
+        else:
+            recommend("Clock")
+    else:
+        print("Enter a valid   difficulty")
+
+
+def recommend(game):
+    print("You might like", game)
+
+
+main()
